58a320e200bc1b19782197a8e99663c89e61d868
David Ding <chengjie.ding@gmail.com>
2014-03-24 12:22:54 +0800

    refactoring logic

--- p001/multiples.c
+++ p001/multiples.c
@@ -8,11 +8,7 @@ int main( void ) {
   long i, total = 0;
 
   for( i = 1; i < THRESHOLD; i++ ) {
-    if( i % ( NUM1 * NUM2 ) == 0 ) { // multiples of both 3 and 5
-      total += i;
-    } else if( i % NUM1 == 0 ) { // multiples of 3 but not 5
-      total += i;
-    } else if( i % NUM2 == 0 ) { // multiples of 5 but not 3
+    if( ( i % NUM1 == 0 ) || ( i % NUM2 == 0 ) ) {
       total += i;
     }
   }
