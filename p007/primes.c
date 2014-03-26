6d7752999f513168f1412967ee688d626affbc49
David Ding <chengjie.ding@gmail.com>
2014-03-26 14:55:31 +0800

    refactoring

--- p007/primes.c
+++ p007/primes.c
@@ -1,99 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <math.h>
 
 #define MAX   10001
-#define SCALE 2
 
-typedef struct ListNode {
-  long long value;
-  struct ListNode *next;
-} list_node;
-
-list_node *make_list_node( const long long value );
-void find_primes( const long long low, const long long high, list_node **primes, int *count );
-void delete_list( list_node **list );
+bool is_prime( const unsigned int number );
 
 int main( void )
 {
-  list_node *primes = NULL;
-  int count = 1;
-  long long low = 1L, high = 2L;
+  int i = 2, count = 1;
 
-  primes = make_list_node( 2L );
-  count = 1;
-
-  while( 1 ) {
-    find_primes( low, high, &primes, &count );
-    low = high;
-    high *= SCALE;
-
-    if( count >= MAX ) {
-      break;
+  while( count != MAX ) {
+    i++;
+    if( is_prime( i ) ) {
+      count++;
     }
   }
-
-  list_node *node = primes;
-  count = 1;
-  while( node ) {
-    if( count++ == MAX ) {
-      printf( "%ld\n", node->value );
-    }
-    node = node->next;
-  }
-
-  delete_list( &primes );
+  printf( "%d\n", i );
 
   return 0;
 }
 
-list_node *make_list_node( const long long value )
+bool is_prime( const unsigned int n )
 {
-  list_node *node = ( list_node * )malloc( sizeof( list_node ) );
-  assert( node );
-  node->value = value;
-  node->next = NULL;
+  int r, f;
 
-  return node;
-}
-
-void find_primes( const long long low, const long long high, list_node **primes, int *count )
-{
-  list_node *tail, *node;
-  long long i;
-
-  for( tail = *primes; tail && tail->next; tail = tail->next )
-    ;
-
-  for( i = low + 1; i <= high; i++ ) {
-    bool found = false;
-    for( node = *primes; node != NULL; node = node->next ) {
-      if( i % node->value == 0 ) {
-	found = true;
-	break;
+  if( n == 1 ) {
+    return false;
+  } else if( n < 4 ) { // 2, 3
+    return true;
+  } else if( n % 2 == 0 ) {
+    return false;
+  } else if( n < 9 ) {
+    return true;
+  } else if( n % 3 == 0 ) {
+    return false;
+  } else {
+    r = floor( sqrt( n ) );
+    f = 5;
+    while( f <= r ) {
+      if( n % f == 0 ) {
+	return false;
       }
+      if( n % ( f + 2 ) == 0 ) {
+	return false;
+      }
+      f += 6;
     }
-    if( !found ) {
-      ( *count )++;
-      tail->next = make_list_node( i );
-      tail = tail->next;
-    }
+    return true;
   }
-}
-
-void delete_list( list_node **list )
-{
-  if( !list )
-    return;
-
-  list_node *temp, *node = *list;
-
-  while( node ) {
-    temp = node;
-    node = node->next;
-    free( temp );
-  }
-
-  *list = NULL;
 }
