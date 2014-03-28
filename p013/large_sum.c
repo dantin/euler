51e02348066fd9aa9cc5d8e13a45a000b514d0fe
David Ding <chengjie.ding@gmail.com>
2014-03-28 23:55:11 +0800

    problem 13: Large sum

--- p013/large_sum.c
+++ p013/large_sum.c
@@ -9,9 +9,8 @@ typedef struct ListNode {
 } list_node;
 
 list_node *make_list_node( const int value );
-void delete_list( list_node **list );
 int read( list_node **list );
-void add( list_node **sum, list_node **number );
+list_node * add( list_node **sum, list_node **number );
 list_node *reverse( list_node *list );
 void print( list_node **list, unsigned int size );
 
@@ -21,9 +20,9 @@ int main( void )
   list_node *number = NULL;
 
   while( read( &number ) != EOF ) {
-    add( &sum, &number );
-    print( &number, 0 );
-    delete_list( &number );
+    //print( &number, 0 );
+    sum = add( &sum, &number );
+    print( &sum, 0 );
   }
 
   print( &sum, 100 );
@@ -39,23 +38,6 @@ list_node *make_list_node( const int value )
   node->next = NULL;
 
   return node;
-}
-
-void delete_list( list_node **list )
-{
-  if( !list ) {
-    return;
-  }
-
-  list_node *temp, *node = *list;
-
-  while( node ) {
-    temp = node;
-    node = node->next;
-    free( temp );
-  }
-
-  *list = NULL;
 }
 
 int read( list_node **list )
@@ -81,13 +63,89 @@ int read( list_node **list )
   return c;
 }
 
-void add( list_node **sum, list_node **number )
+list_node *add( list_node **num_a, list_node **num_b )
 {
+  list_node *pa, *pb, *node, *tail, *temp;
+  list_node *sum;
 
+  if( *num_a == NULL ) {
+    pb = *num_b;
+    tail = NULL;
+    while( pb ) {
+      node = make_list_node( pb->value );
+      if( tail ) {
+	tail->next = node;
+	tail = node;
+      } else {
+	sum = node;
+	tail = node;
+      }
+      temp = pb;
+      pb = pb->next;
+      free( temp );
+    }
+    *num_b = NULL;
+  } else {
+    sum = NULL;
+    tail = NULL;
+
+    pa = reverse( *num_a );
+    pb = reverse( *num_b );
+
+    int remaining = 0;
+    while( pa || pb ) {
+      int a = pa ? pa->value : 0;
+      int b = pb ? pb->value : 0;
+
+      node = make_list_node( ( a + b + remaining ) % 10 );
+      remaining = ( a + b + remaining) / 10;
+
+      if( sum ) {
+	node->next = sum;
+	sum = node;
+      } else {
+	sum = node;
+      }      
+
+      if( pa ) {
+	temp = pa;
+	pa = pa->next;
+	free( temp );
+      }
+      if( pb ) {
+	temp = pb;
+	pb = pb->next;
+	free( temp );
+      }
+    }
+
+    while( remaining ) {
+      node = make_list_node( remaining % 10 );
+      node->next = sum;
+      sum = node;
+      remaining /= 10;
+    }
+
+    *num_a = NULL;
+    *num_b = NULL;
+  }
+
+  return sum;
 }
 
 list_node *reverse( list_node *list )
 {
+  list_node *reversed, *temp;
+
+  reversed = NULL;
+  while( list ) {
+    temp = list;
+    list = list->next;
+    temp->next = reversed;
+    reversed = temp;
+  }
+
+  return reversed;
 }
 
 void print( list_node **list, unsigned int size )
