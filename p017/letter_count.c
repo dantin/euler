8c52720900e5bc9493a867d11c2088f618206c97
David Ding <chengjie.ding@gmail.com>
2014-03-31 23:33:24 +0800

    add basic structure

---
+++ p017/letter_count.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char *digit[10] = {
+  "",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine" };
+
+
+int main( void )
+{
+  return 0;
+}
