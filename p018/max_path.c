5033e9ab43d20a4db43a9f128f07b100d7003025
David Ding <chengjie.ding@gmail.com>
2014-04-01 17:44:52 +0800

    add list path function

--- p018/max_path.c
+++ p018/max_path.c
@@ -12,6 +12,8 @@ typedef struct Node {
 node *make_node( const int value );
 void destory_list( node **list );
 void print( node *list );
+node *copy_list( node *list );
+node *reverse_list( node *list );
 
 int main( void )
 {
@@ -50,14 +52,28 @@ int main( void )
 	pc = plist;
 	cc = clist;
 	while( cc ) {
-	  if( pp == NULL ) {
+	  if( pp == NULL && pc == NULL ) {
+	    // will not happen
+	  } else if( pp == NULL && pc != NULL ) {
+	    pn = make_node( pc->value );
+	    pn->next = copy_list( pc->path );
 	    cc->weight = pc->weight + cc->value;
-	  } else if( pc == NULL ) {
+	    cc->path = pn;
+	  } else if( pc == NULL && pp != NULL) {
+	    pn = make_node( pp->value );
+	    pn->next = copy_list( pp->path );
 	    cc->weight = pp->weight + cc->value;
+	    cc->path = pn;
 	  } else if( pp->weight > pc->weight ) {
+	    pn = make_node( pp->value );
+	    pn->next = copy_list( pp->path );
 	    cc->weight = pp->weight + cc->value;
+	    cc->path = pn;
 	  } else {
+	    pn = make_node( pc->value );
+	    pn->next = copy_list( pc->path );
 	    cc->weight = pc->weight + cc->value;
+	    cc->path = pn;
 	  }
 
 	  pp = pc;
@@ -74,13 +90,23 @@ int main( void )
   }
 
   int max = 0;
+  node *max_node = NULL;
+  node *path;
   pn = plist;
   while( pn ) {
     if( max < pn->weight ) {
       max = pn->weight;
+      max_node = pn;
     }
     pn = pn->next;
   }
+
+  path = make_node( max_node->value );
+  path->next = copy_list( max_node->path );
+  path = reverse_list( path );
+  print( path );
+
+  destory_list( &path );
   destory_list( &plist );
 
   printf( "max total: %d\n", max );
@@ -111,6 +137,9 @@ void destory_list( node **list )
   cp = *list;
   while( cp ) {
     tp = cp;
+    if( tp->path ) {
+      destory_list( &( tp->path ) );
+    }
     cp = cp->next;
     free( tp );
   }
@@ -124,4 +153,40 @@ void print( node *list )
     printf( "%d ", list->value );
     list = list->next;
   }
+}
+
+node *copy_list( node *list )
+{
+  node *copy = NULL;
+  node *tail, *node;
+
+  while( list ) {
+    node = make_node( list->value );
+    if( copy == NULL ) {
+      copy = node;
+      tail = node;
+    } else {
+      tail->next = node;
+      tail = node;
+    }
+
+    list = list->next;
+  }
+
+  return copy;
+}
+
+node *reverse_list( node *list )
+{
+  node *reversed = NULL;
+  node *n;
+
+  while( list ) {
+    n = list;
+    list = list->next;
+    n->next = reversed;
+    reversed = n;
+  }
+
+  return reversed;
 }
