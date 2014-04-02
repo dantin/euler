105ad485f61218425a2440ae75be131196e7d724
David Ding <chengjie.ding@gmail.com>
2014-04-02 12:21:54 +0800

    using int instead of long to store sum value

--- p001/multiples.c
+++ p001/multiples.c
@@ -5,15 +5,15 @@
 #define THRESHOLD 1000
 
 int main( void ) {
-  long i, total = 0;
+  int total = 0;
 
-  for( i = 1; i < THRESHOLD; i++ ) {
+  for( int i = 1; i < THRESHOLD; i++ ) {
     if( ( i % NUM1 == 0 ) || ( i % NUM2 == 0 ) ) {
       total += i;
     }
   }
 
-  printf( "%ld\n", total );
+  printf( "%d\n", total );
 
   return 0;
 }
